98b67b3a5b64f09d871c978dc145ce7421d330b5
andykr1k <69021102+andykr1k@users.noreply.github.com>
2024-02-12 23:37:23 -0800

    Added Some Classes

--- src/Tokenizer.hpp
+++ src/Tokenizer.hpp
@@ -1,13 +1,44 @@
 #include "Token.hpp"
 
-vector<Token> Tokenize(const string& str){
-    vector<Token> tokens;
-    
-    for (int i = 0; i < str.length(); i++) {
-        
-        cout << str[0] << endl;
+bool is_digits(const string& str) {
+    for (char ch : str) {
+        int v = ch;
+        if (!(ch >= 48 && ch <= 57)) {
+            return false;
+        }
     }
-
-    return tokens;
+    return true; 
 }
 
+Token createToken(const string& buf){
+    Token token;
+    if (!is_digits(buf)) {
+        if ( buf == "return" ) {
+            token._type = TokenType::_return;
+        } else if ( buf == ";" ) {
+            token._type = TokenType::_semi;
+        }
+    } else {
+        token._type = TokenType::_int;
+    }
+    token._value = buf;
+
+    return token;
+}
+
+vector<Token> Tokenize(const string& str){
+    
+    vector<Token> tokens;
+    string buf;
+
+    for (int i = 0; i < str.length(); i++) {        
+        if (str[i] != ' '){
+            buf.push_back(str[i]);
+        } else {
+            tokens.push_back(createToken(buf));
+            buf = "";
+        }
+    }
+    
+    return tokens;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,28 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
 
 #include "Tokenizer.hpp"
 
 int main() {
    
-    fstream inputFile;
-    
     string file_path = "./inputs/input.txt";
-
-    inputFile.open(file_path, ios::in);
 
     string file_contents;
 
     {
-        if (inputFile.is_open()) {
-            while (getlineminputFile, file_contents)) {
-                file_contents << inputFile;
-            }
-        } else {
-            cout << "Error: Could Not Open File!" << endl;
-            return EXIT_FAILURE;
-        } 
+        stringstream file_contents_stream;
+        fstream input(file_path, ios::in);
+        file_contents_stream << input.rdbuf();
+        file_contents = file_contents_stream.str();
     }
 
     vector<Token> tokens = Tokenize(file_contents);
     
+    for ( int i = 0; i < tokens.size(); i++){
+        cout << tokens[i]._value << endl;
+    }
+
     return EXIT_SUCCESS;
 }
