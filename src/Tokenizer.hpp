8b43df3a78db6e8a5bb6a7c662915ab521da2e67
andykr1k <69021102+andykr1k@users.noreply.github.com>
2024-02-16 12:07:20 -0800

    Made Tokenizer Class - Transfer Computers

--- src/Tokenizer.hpp
+++ src/Tokenizer.hpp
@@ -1,44 +1,63 @@
 #include "Token.hpp"
 
-bool is_digits(const string& str) {
-    for (char ch : str) {
-        int v = ch;
-        if (!(ch >= 48 && ch <= 57)) {
-            return false;
+class Token(const string src){
+    public:
+        inline vector<Token> Tokenize(const string& str){
+
+            vector<Token> tokens;
+            string buf;
+
+            for (int i = 0; i < str.length(); i++) {        
+                if (str[i] != ' '){
+                    buf.push_back(str[i]);
+                } else {
+                    tokens.push_back(createToken(buf));
+                    buf = "";
+                }
+            }
+
+            return tokens;
         }
-    }
-    return true; 
-}
 
-Token createToken(const string& buf){
-    Token token;
-    if (!is_digits(buf)) {
-        if ( buf == "return" ) {
-            token._type = TokenType::_return;
-        } else if ( buf == ";" ) {
-            token._type = TokenType::_semi;
+    private:
+
+        string file_source = src;
+        int file_index = 0;
+
+        inline char peak(offet = 1){
+            if (file_index + offset <= file_source.size()) {
+                return src[file_index + offset]; 
+            }
         }
-    } else {
-        token._type = TokenType::_int;
-    }
-    token._value = buf;
 
-    return token;
-}
-
-vector<Token> Tokenize(const string& str){
-    
-    vector<Token> tokens;
-    string buf;
-
-    for (int i = 0; i < str.length(); i++) {        
-        if (str[i] != ' '){
-            buf.push_back(str[i]);
-        } else {
-            tokens.push_back(createToken(buf));
-            buf = "";
+        inline char consume(){
+            return src[file_index++];
         }
-    }
-    
-    return tokens;
+
+        inline Token createToken(const string& buf){
+            Token token;
+            if (!is_digits(buf)) {
+                if ( buf == "return" ) {
+                    token._type = TokenType::_return;
+                } else if ( buf == ";" ) {
+                    token._type = TokenType::_semi;
+                }
+            } else {
+                token._type = TokenType::_int;
+            }
+            token._value = buf;
+
+            return token;
+        }
+
+        inline bool is_digits(const string& str) {
+            for (char ch : str) {
+                int v = ch;
+                if (!(ch >= 48 && ch <= 57)) {
+                    return false;
+                }
+            }
+            return true; 
+        }
+
 }
